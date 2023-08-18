484f34ff7c052044fa9226af9a8ec2033e7c7358
nirmal499 <nbaskey499@gmail.com>
2023-08-19 00:16:05 +0530

    Separated the common functions into utils.py file

--- ai_algorithms/maze_problem_using_bfs.py
+++ ai_algorithms/maze_problem_using_bfs.py
@@ -1,47 +1,14 @@
-from pyamaze import maze
-
-class MyCell:
-    def __init__(self, my_tuple):
-        self.my_tuple = my_tuple
-        self.parent = None
-
-    def set_parent(self, parent_cell):
-        self.parent = parent_cell
-
-    def __str__(self):
-        return str(self.my_tuple)
-
-def is_in_visited_cells(neighbor, visited_cells):
-    for cells in visited_cells:
-        if neighbor.my_tuple == cells.my_tuple:
-            return True
-
-    return False
-
-def my_print(cells_arr):
-    print("[",end=" ")
-    for cell in cells_arr:
-        print(cell, end=", ")
-
-    print("]")
-
-# Utility to get a path as a list of points by traversing the parents of a node until the root is reached.
-def get_path(point):
-    path = []
-    current_point = point
-    while current_point.parent is not None:
-        path.append(current_point)
-        current_point = current_point.parent
-    return path
+from libs.pyamaze import maze
+import libs.utils as utils
 
 def run_bfs(m, visited_cells):
     q = []
     # q.append((m.rows, m.cols))
 
-    q.append(MyCell((m.rows, m.cols)))
+    q.append(utils.MyCell((m.rows, m.cols)))
 
     # marking current_point as visited
-    visited_cells.append(MyCell((m.rows, m.cols)))
+    visited_cells.append(utils.MyCell((m.rows, m.cols)))
 
     # bfsPath = dict()
 
@@ -56,20 +23,20 @@ def run_bfs(m, visited_cells):
                 # available_point = tuple()
 
                 if d == 'E':
-                    available_point = MyCell((current_point_cell.my_tuple[0], current_point_cell.my_tuple[1] + 1))
+                    available_point = utils.MyCell((current_point_cell.my_tuple[0], current_point_cell.my_tuple[1] + 1))
                 if d == 'W':
-                    available_point = MyCell((current_point_cell.my_tuple[0], current_point_cell.my_tuple[1] - 1))
+                    available_point = utils.MyCell((current_point_cell.my_tuple[0], current_point_cell.my_tuple[1] - 1))
                 if d == 'N':
-                    available_point = MyCell((current_point_cell.my_tuple[0] - 1, current_point_cell.my_tuple[1]))
+                    available_point = utils.MyCell((current_point_cell.my_tuple[0] - 1, current_point_cell.my_tuple[1]))
                 if d == 'S':
-                    available_point = MyCell((current_point_cell.my_tuple[0] + 1, current_point_cell.my_tuple[1]))
+                    available_point = utils.MyCell((current_point_cell.my_tuple[0] + 1, current_point_cell.my_tuple[1]))
 
                 neighbors_cells.append(available_point)
 
         # my_print(neighbors_cells)
 
         for neighbor in neighbors_cells:
-            if not is_in_visited_cells(neighbor, visited_cells):
+            if not utils.is_in_visited_cells(neighbor, visited_cells):
                 # current_point_cell = neighbor
 
                 neighbor.set_parent(current_point_cell)
@@ -98,7 +65,7 @@ outcome = run_bfs(m,[])
 if type(outcome) is str:
     print(outcome)
 else:
-    bfs_path = get_path(outcome)
-    my_print(bfs_path)
+    bfs_path = utils.get_path(outcome)
+    utils.my_print(bfs_path)
 
 m.run()
